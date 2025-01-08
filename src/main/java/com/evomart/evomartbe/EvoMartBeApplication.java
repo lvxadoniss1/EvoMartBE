7a352a307b4bb8f7ca4342504ba8e69574d1e946
Adonis <tgbao2306@gmail.com>
2025-01-09 01:08:27 +0700

    first commit

---
+++ src/main/java/com/evomart/evomartbe/EvoMartBeApplication.java
@@ -0,0 +1,13 @@
+package com.evomart.evomartbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoMartBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoMartBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evomart/evomartbe/EvoMartBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evomart.evomartbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoMartBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
